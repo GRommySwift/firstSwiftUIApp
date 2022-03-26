4a1d980b97badafa25234eb774c1ee1c9b90d389
Roman <romaholovai@gmail.com>
2022-03-26 21:43:52 +0100

    Initial Commit

---
+++ firstSwiftUIApp/firstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftUIAppApp.swift
+//  firstSwiftUIApp
+//
+//  Created by Roman Holovai on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
